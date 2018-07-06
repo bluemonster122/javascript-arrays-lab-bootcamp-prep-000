143e75a1129ab7fe1e30469d1f163ca45116b967
L H Gilfoyle <l.gilfoyle@wlv.ac.uk>
2018-07-06 13:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ function destructivelyPrependKitten(name) { kittens.unshift(name) }
 function destructivelyRemoveFirstKitten() { kittens.shift() }
 function appendKitten(name) { return [...kittens, name] }
 function prependKitten(name) { return [name, ...kittens] }
-function removeLastKitten(name) { return kittens.slice(0, kittens.length) }
+function removeLastKitten(name) { return kittens.slice(0, kittens.length - 1) }
+function removeFirstKitten(name) { return kittens.slice(1) }

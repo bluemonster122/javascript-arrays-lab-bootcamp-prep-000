02e3838ee9c3fbdcc313c26c334c731ce8aaea5b
L H Gilfoyle <l.gilfoyle@wlv.ac.uk>
2018-07-06 13:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Add your functions and code here
 kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
-  kittens.push(name);
-}
-
-function destructivelyRemoveLastKitten() {
-  kittens.pop();
-}
+function destructivelyAppendKitten(name) { kittens.push(name) }
+function destructivelyRemoveLastKitten() { kittens.pop() }
+function destructivelyPrependKitten(name) { kittens.unshift(name) }
+function destructivelyRemoveFirstKitten() { kittens.shift() }
+function appendKitten(name) { return [...kittens, name] }
+function prependKitten(name) { return [name, ...kittens] }
+function removeLastKitten(name) { return kittens.slice(1) }

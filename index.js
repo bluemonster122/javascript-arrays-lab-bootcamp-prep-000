a009394161a107be62d56ccd2ec450f4aadfb687
L H Gilfoyle <l.gilfoyle@wlv.ac.uk>
2018-07-06 13:46:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
